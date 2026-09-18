94c99b0ac37b26cb9a7441eef8f5a6b1c5517b81
rebecacalvouquintero <rebeca.calvoquintero@elephant.healthcare>
2026-09-18 14:43:15 +0200

    more tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
         <button type="button" onClick={scrollToTop} className="site-footer__top">
           Back to top
           <span className="site-footer__top-arrow" aria-hidden="true">
-            <ArrowIcon direction="up" size={12} strokeWidth={1} />
+            <ArrowIcon direction="up" size={15} strokeWidth={1.35} />
           </span>
         </button>
       </div>
